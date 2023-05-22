2161f6ffc8fbe8a00dca82be11b8826dc8fd2e01
Xirdman <59977898+Xirdman@users.noreply.github.com>
2023-05-22 11:52:11 +0400

    Update ExchangeService.kt

--- src/main/kotlin/com/dashboards/peredelano/service/exchange/ExchangeService.kt
+++ src/main/kotlin/com/dashboards/peredelano/service/exchange/ExchangeService.kt
@@ -1,6 +1,6 @@
 package com.dashboards.peredelano.service.exchange
 
-import com.dashboards.peredelano.api.model.exchange.ExchageCryptoResponse
+import com.dashboards.peredelano.api.model.exchange.ExchageResponse
 import com.dashboards.peredelano.web.dto.exchange.Currency
 import com.dashboards.peredelano.web.dto.exchange.ExchangeRatio
 import com.dashboards.peredelano.web.dto.exchange.ExchangeRequest
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.math.BigDecimal
 
+/**
+ * Сервис конвертации валют.
+ */
 @Service
 class ExchangeService(
     private val exchangeRatioRestTemplate: RestTemplate
@@ -19,45 +22,54 @@ class ExchangeService(
      * Получение Курса обмена из exchangerate.com.
      */
     fun getExchange(request: ExchangeRequest): Collection<ExchangeRatio> {
-        val containUSDT = request.currenciesTo.contains(Currency.USDT)
-        val currenciesWithoutUSDT = request.currenciesTo.filter { it != Currency.USDT }
-        val result = getExchangeWithoutUSDT(
-            currenciesWithoutUSDT,
+        return getRates(
+            request.currenciesTo.map { getExchangeName(it) },
             request.amountFrom,
             request.currencyFrom
-        )
-        if (containUSDT) {
-            result.add(
+        )?.let { rates ->
+            request.currenciesTo.mapNotNull { currencyRate ->
+                getExchangeRatio(request, currencyRate, rates)
+            }
+        } ?: emptyList()
+    }
+
+    private fun getRates(
+        currenciesNameAtExchange: List<String>,
+        amount: BigDecimal,
+        base: Currency
+    ): Map<String, BigDecimal>? {
+        if (currenciesNameAtExchange.isEmpty()) {
+            return emptyMap()
+        }
+        val symbols = currenciesNameAtExchange
+            .toSet()
+            .joinToString(",")
+        val uri = "https://api.exchangerate.host/latest?base=$base&symbols=$symbols&amount=$amount"
+        val rates = exchangeRatioRestTemplate.getForEntity(uri, ExchageResponse::class.java)
+        return rates.body?.rates
+    }
+
+    private fun getExchangeRatio(
+        request: ExchangeRequest,
+        currency: Currency,
+        rates: Map<String, BigDecimal>
+    ): ExchangeRatio? {
+        val name = getExchangeName(currency)
+        return rates[name]
+            ?.let {
                 ExchangeRatio(
                     currencyFrom = request.currencyFrom,
                     amountFrom = request.amountFrom,
-                    currencyTo = Currency.USDT,
-                    amountTo = request.amountFrom
+                    currencyTo = currency,
+                    amountTo = it
                 )
-            )
-        }
-        return result
+            }
     }
 
-    private fun getExchangeWithoutUSDT(
-        currenciesWithoutUSDT : List<Currency>,
-        amount: BigDecimal,
-        base: Currency
-    ): MutableList<ExchangeRatio> {
-        if (currenciesWithoutUSDT.isEmpty()) {
-            return mutableListOf()
+    private fun getExchangeName(currency: Currency): String {
+        if (currency == Currency.USDT) {
+            return Currency.USD.toString()
         }
-        val symbols = currenciesWithoutUSDT.joinToString(",")
-        val uri = "https://api.exchangerate.host/latest?base=$base&symbols=$symbols&amount=$amount"
-        val rates = exchangeRatioRestTemplate.getForEntity(uri, ExchageCryptoResponse::class.java)
-        return rates?.body
-            ?.rates
-            ?.mapNotNull { rate -> ExchangeRatio(
-                currencyFrom = base,
-                amountFrom = amount,
-                currencyTo = Currency.valueOf(rate.key),
-                amountTo = rate.value
-            ) }?.toMutableList()
-            ?: mutableListOf()
+        return currency.toString()
     }
 }
